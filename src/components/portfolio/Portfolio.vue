3068cd3da3458970816e8f6aec7d5deed5156aac
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-08 13:04:50 +0200

    connecting the portfolio with vuex

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>The Portfolio  component</h1>
+<div>
+  <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+</div>
 </template>
+<script>
+import { mapGetters } from "vuex";
+import Stock from "./Stock.vue";
+export default {
+  components: { appStock: Stock },
+  computed: {
+    ...mapGetters({
+      stocks: "stockPortfolio"
+    })
+  }
+};
+</script>
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -35,6 +35,7 @@ export default {
         quantity: this.quantity
       };
       console.log(order);
+      this.$store.dispatch("buyStock", order);
       this.quantity = 0;
     }
   }
