278a21eb248c5c8cea9fe629ef3e6c05a0d639eb
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-03-09 13:53:33 +0200

    vue resource imported and firebase connected

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 import store from './store/store';
 Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.http.options.root = 'https://vuejs-stock-trader-9f916.firebaseio.com/';
 Vue.filter('currency', (value) => {
     return '$' + value.toLocaleString();
 });
